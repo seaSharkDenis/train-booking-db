4c7a2036897518173141d4236b27409482ba68d3
Denis Ndiritu <denisndiritu1@gmail.com>
2025-03-29 23:43:36 +0300

    Table Creation Updates Changes Made -	Added SQL queries to create new tables in the trainbookingDB database Details of changes 1.	Created the following tables: a.	booking_status: Incldes the id and name columns. b.	train_station: Includes the id, and station_name columns. c.	passenger: Includes the id, first_name, last_name, email_address, and password columns. d.	schedule: Includes the id and name columns. e.	carriage_class: Includes the id, class_name and seating_capacity columns. f.	train_journey: Includes the id, schedule_id, and name columns. g.	journey_carriage: Includes the journey_id, carriage_class_id, and position columns. h.	journey_station: Includes the journey_id, station_id, and stop_order columns. i.	journey_price: Includes the schedule_id, carriage_class_id and price columns.
    2.	Edited the following table:
    a.	booking: Includes the id, passenger_id, status_id, booking_date, starting_station_id, ending_station_id, train_journey_id, ticket_class_id, amount_paid, ticket_no and seat_no columns.
    
    3.	SQL Statements Used:
    a.	Utilized the CREATE TABLE statement to define table structures.
    b.	Added necessary constraints: primary keys (PRIMARY KEY), foreign keys (FOREIGN KEY) and not null (NOT NULL) for referential integrity.

--- db.sql
+++ db.sql
@@ -1,29 +1,80 @@
 CREATE DATABASE trainbookingDB;
 
-
-
-
-
-
-CREATE TABLE booking (
-    id INT NOT NULL PRIMARY KEY,
-    carriage_class_id INT,
-	passenger_id INT, 
-    position INT,
-	status_id INT,  
-	booking_date TIMESTAMP, 
-	starting_station_id INT, 
-	ending_station_id INT, 
-	train_journey_id INT, 
-	ticket_class_id INT,
-	amount_paid INT,
-	ticket_no INT,
-	seat_no INT
-    FOREIGN KEY (passenger_id) REFERENCES passenger(id),
-    FOREIGN KEY (status_id) REFERENCES booking_status(id),
-	FOREIGN KEY (starting_station_id) REFERENCES train_station(id),
-	FOREIGN KEY (ending_station_id) REFERENCES train_station(id),
-	FOREIGN KEY (train_journey_id) REFERENCES journey_station(id),
-	FOREIGN KEY (ticket_class_id) REFERENCES carriage_class(id),
+CREATE TABLE booking_status (
+	id INT PRIMARY NOT NULL,
+	name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE train_station (
+	id INT PRIMARY KEY NOT NULL,
+	station_name varchar(255) NOT NULL
+);
+
+CREATE TABLE passenger (
+	id INT NOT NULL PRIMARY KEY,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	email_address VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE schedule (
+	id INT PRIMARY KEY NOT NULL,
+	name varchar(255) NOT NULL
+);
+
+CREATE TABLE carriage_class (
+	id INT PRIMARY KEY NOT NULL,
+	class_name varchar(255) NOT NULL,
+	seating_capacity INT NOT NULL
+);
+
+CREATE TABLE train_journey (
+	id INT PRIMARY KEY NOT NULL,
+	schedule_id INT NOT NULL,
+	name INT NOT NULL,
+	FOREIGN KEY (schedule_id) REFERENCES schedule (id)
+);
+
+CREATE TABLE journey_carriage (
+	journey_id INT NOT NULL,
+	carriage_class_id INT NOT NULL,
+	position varchar(255) NOT NULL,
+	FOREIGN KEY (journey_id) REFERENCES train_journey (id),
+	FOREIGN KEY (carriage_class_id) REFERENCES carriage_class (id)
+);
+
+CREATE TABLE journey_station (
+	journey_id INT NOT NULL,
+	station_id INT NOT NULL,
+	stop_order varchar(255) NOT NULL,
+	FOREIGN KEY (journey_id) REFERENCES train_journey (id),
+	FOREIGN KEY (station_id) REFERENCES train_station (id)
+);
+
+CREATE TABLE journey_price (
+	schedule_id INT NOT NULL,
+	carriage_class_id INT NOT NULL,
+	price varchar(255) NOT NULL,
+	FOREIGN KEY (schedule_id) REFERENCES schedule (id)
+);
+
+CREATE TABLE booking (
+	id INT PRIMARY KEY NOT NULL,
+	passenger_id INT NOT NULL,
+	status_id INT NOT NULL,
+	booking_date TIMESTAMP,
+	starting_station_id INT NOT NULL,
+	ending_station_id INT NOT NULL,
+	train_journey_id INT NOT NULL,
+	ticket_class_id INT NOT NULL,
+	amount_paid INT NOT NULL,
+	ticket_no varchar(255),
+	seat_no INT,
+	FOREIGN KEY (passenger_id) REFERENCES passenger (id),
+	FOREIGN KEY (status_id) REFERENCES booking_status (id),
+	FOREIGN KEY (starting_station_id) REFERENCES train_station (id),
+	FOREIGN KEY (ending_station_id) REFERENCES train_station (id),
+	FOREIGN KEY (train_journey_id) REFERENCES train_journey (id),
+	FOREIGN KEY (ticket_class_id) REFERENCES carriage_class (id)
+);
